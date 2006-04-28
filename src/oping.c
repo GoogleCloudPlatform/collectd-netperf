dd034fad465d2e5c9fe955194141d04edf43079b
octo <octo>
2006-04-28 21:35:32 +0000

    `oping' now compiles and I'll go to bed. G'night.

--- src/oping.c
+++ src/oping.c
@@ -1,7 +1,37 @@
-#include <stdlib.h>
-#include <stdio.h>
+/**
+ * Object oriented C module to send ICMP and ICMPv6 `echo's.
+ * Copyright (C) 2006  Florian octo Forster <octo at verplant.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
 
-#include "liboping.h"
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#if STDC_HEADERS
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <errno.h>
+# include <assert.h>
+#else
+# error "You don't have the standard C99 header files installed"
+#endif /* STDC_HEADERS */
+
+#include <liboping.h>
 
 int main (int argc, char **argv)
 {
