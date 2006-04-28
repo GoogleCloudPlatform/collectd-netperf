c6d4c6ef8f98385cc718af2ec13bc25700e75e20
octo <octo>
2006-04-28 21:02:35 +0000

    Added auto{conf,make} files..

--- src/liboping.c
+++ src/liboping.c
@@ -48,6 +48,12 @@
 #if TIME_WITH_SYS_TIME
 # include <sys/time.h>
 # include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
 #endif
 
 #if HAVE_SYS_SOCKET_H
